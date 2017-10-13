209af0897ba28ad16659ad54c69440e53d7f8e82
Mohamed Ibrahim <mohamed@attitudetech.ie>
2017-10-13 14:29:05 +0100

    Fixed the behaviour of the sliders and added ScrollView to work on smaller screens.

--- PawTrails/SetUpYourGoalController.swift
+++ PawTrails/SetUpYourGoalController.swift
@@ -21,15 +21,26 @@ class SetUpYourGoalController: UIViewController {
     
     
     var adventureSlidelabel: UILabel?
+    var playingSliderLabel: UILabel?
+    var nomalSliderLabel: UILabel?
+    var restingSliderLabel: UILabel?
     
-    var slider1:Float = 0.0
-    var slider2:Float = 0.0
+    var nomralSliderValue:Float = 0.0
+    var playingSliderValue:Float = 0.0
+    var restingSliderValue: Float = 0.0
+    var adventureSliderValue: Float = 0.0
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+       
         restToDefult.backgroundColor = UIColor.primary
         restToDefult.fullyroundedCorner()
+        
+        self.popUpDestructive(title: "Note", msg: "The value of resting, normal and playing goals must be 24 hours", cancelHandler: nil) { (proceed) in
+            
+        }
         
     }
     
@@ -40,24 +51,75 @@ class SetUpYourGoalController: UIViewController {
         
         let image = UIImage(named: "SliderBtn")?.scaleToSize(newSize: CGSize(width: 70, height: 40))
         
-        
-        
         adventureSlider.setThumbImage(image, for: .normal)
         playngSlider.setThumbImage(image, for: .normal)
         NormalGoalSlider.setThumbImage(image, for: .normal)
         restingGoalSlider.setThumbImage(image, for: .normal)
+        
+        let restingColors = UIColor(red: 153/255, green: 202/255, blue: 186/255, alpha: 1)
+        let normalColors = UIColor(red: 211/255, green: 100/255, blue: 59/255, alpha: 1)
+        let playingColors = UIColor(red: 67/255, green: 62/255, blue: 54/255, alpha: 1)
+        let adventureColor = UIColor(red: 108/255, green: 176/255, blue: 255/255, alpha: 1)
+
 
     
         
         if let handleView = adventureSlider.subviews.last as? UIImageView {
             let label = UILabel(frame: handleView.bounds)
             label.backgroundColor = UIColor.clear
+            label.textAlignment = .center
+            label.textColor = adventureColor
+
+            handleView.addSubview(label)
+            self.adventureSlidelabel = label
+//            let value = self.adventureSlider.value
+            self.adventureSlidelabel?.text = "\(adventureSliderValue)"
+        }
+        
+        
+        
+        
+        if let handleView = playngSlider.subviews.last as? UIImageView {
+            let label = UILabel(frame: handleView.bounds)
+            label.backgroundColor = UIColor.clear
+            label.textAlignment = .center
+            label.textColor = playingColors
+
+
             handleView.addSubview(label)
             
-            self.adventureSlidelabel = label
-            let value = self.adventureSlider.value
-            self.adventureSlidelabel?.text = "\(value)"
+            self.playingSliderLabel = label
+            let value = self.playngSlider.value
+            self.playingSliderLabel?.text = "\(value)"
 
+            
+        }
+        
+        
+        if let handleView = restingGoalSlider.subviews.last as? UIImageView {
+            let label = UILabel(frame: handleView.bounds)
+            label.backgroundColor = UIColor.clear
+            label.textAlignment = .center
+            handleView.addSubview(label)
+            label.textColor = restingColors
+
+            
+            self.restingSliderLabel = label
+//            let value = self.restingGoalSlider.value
+            self.restingSliderLabel?.text = "\(restingSliderValue)"
+        }
+        
+        
+        if let handleView = NormalGoalSlider.subviews.last as? UIImageView {
+            let label = UILabel(frame: handleView.bounds)
+            label.backgroundColor = UIColor.clear
+            label.textAlignment = .center
+            label.textColor = normalColors
+
+
+            handleView.addSubview(label)
+            self.nomalSliderLabel = label
+            self.nomalSliderLabel?.text = "\(nomralSliderValue)"
         }
 
     }
@@ -66,23 +128,59 @@ class SetUpYourGoalController: UIViewController {
     
     
     
+    
+    
     @IBAction func adventureAction(_ sender: UISlider) {
-
+        
+        adventureSliderValue = sender.value
+        adventureSlidelabel?.text = "\(Int(sender.value)) H"
+        
+      
     }
     
     @IBAction func playingAction(_ sender: UISlider) {
+        
+        sender.maximumValue = 24 - (restingSliderValue + nomralSliderValue)
+
+        playingSliderValue = sender.value
+        playingSliderLabel?.text = "\(Int(sender.value)) H"
+
+
 
     }
     
-    @IBAction func restingAction(_ sender: Any) {
+    @IBAction func restingAction(_ sender: UISlider) {
+        sender.maximumValue = 24 - (nomralSliderValue + playingSliderValue)
+        restingSliderValue = sender.value
+        restingSliderLabel?.text = "\(Int(sender.value)) H"
+
     }
     
     
-    @IBAction func nomalAction(_ sender: Any) {
+    @IBAction func nomalAction(_ sender: UISlider) {
+        sender.maximumValue = 24 - (restingSliderValue + playingSliderValue)
+        nomralSliderValue = sender.value
+        nomalSliderLabel?.text = "\(Int(sender.value)) H"
+
+
     }
    
+    @IBAction func SaveBtnPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    
     @IBAction func restButtonPressed(_ sender: Any) {
-        self.adventureSlidelabel?.text = "fds"
+        self.adventureSlider.setValue(0.0, animated: true)
+        self.playngSlider.setValue(0.0, animated: true)
+        self.restingGoalSlider.setValue(0.0, animated: true)
+        self.NormalGoalSlider.setValue(0.0, animated: true)
+        
+        
+        adventureSlidelabel?.text = "\(Int(adventureSlider.value)) H"
+        playingSliderLabel?.text = "\(Int(playngSlider.value)) H"
+        restingSliderLabel?.text = "\(Int(restingGoalSlider.value)) H"
+        nomalSliderLabel?.text = "\(Int(NormalGoalSlider.value)) H"
+
 
     }
 
