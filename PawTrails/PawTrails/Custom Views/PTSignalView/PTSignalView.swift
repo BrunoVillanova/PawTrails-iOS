20a757e2ed729234ff20d4c40f8282726b8b635f
Bruno Villanova <bruno@digimundi.com.br>
2017-10-23 11:01:26 -0200

    added signal view to pet map callout

---
+++ PawTrails/PawTrails/Custom Views/PTSignalView/PTSignalView.swift
@@ -0,0 +1,45 @@
+//
+//  PTSignalView.swift
+//  PawTrails
+//
+//  Created by Bruno Villanova on 23/10/17.
+//  Copyright © 2017 AttitudeTech. All rights reserved.
+//
+
+import UIKit
+
+class PTSignalView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    let signalFullIcon = UIImage(named: "SignalFull")
+    let imageView = UIImageView(frame: CGRect(x: 0, y:0, width: 20, height: 10))
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    fileprivate func setup() {
+        
+        self.backgroundColor = UIColor.clear
+        
+        // Create the imageView with the battery image and add it to the view
+        imageView.image = signalFullIcon
+        imageView.contentMode = UIViewContentMode.scaleAspectFit
+        imageView.frame = CGRect(x: 0, y:0, width: self.frame.width, height: self.frame.height)
+        imageView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        self.addSubview(imageView)
+    }
+}
