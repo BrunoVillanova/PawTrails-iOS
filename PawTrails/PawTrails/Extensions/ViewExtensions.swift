6b823671e3c83635687a6be8e18a52f4901026bc
Bruno Villanova <bruno@digimundi.com.br>
2017-12-10 15:37:43 -0200

    layout: changed font and size from trip alert

--- PawTrails/PawTrails/Extensions/ViewExtensions.swift
+++ PawTrails/PawTrails/Extensions/ViewExtensions.swift
@@ -188,7 +188,7 @@ extension UIViewController {
         let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: viewHeight))
         label.text = title
         label.textColor = UIColor.white
-        label.font = UIFont.preferredFont(forTextStyle: .callout)
+        label.font = UIFont.systemFont(ofSize: 10, weight: UIFontWeightBold)
         label.textAlignment = .center
         label.adjustsFontSizeToFitWidth = true
         label.numberOfLines = 2
