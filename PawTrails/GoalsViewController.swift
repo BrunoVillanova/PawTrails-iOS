936532b93ef6f21e886e92a4cf4530fc73bfb140
Bruno Villanova <bruno@digimundi.com.br>
2017-12-10 03:40:38 -0200

    - Updated LaunchScreens - Charts using Carthage - Added build bump

--- PawTrails/GoalsViewController.swift
+++ PawTrails/GoalsViewController.swift
@@ -9,9 +9,6 @@ import UIKit
 import XLPagerTabStrip
 import Charts
 
-
-
-
 class GoalsViewController: UIViewController, IndicatorInfoProvider, ChartViewDelegate, DateDelegate {
   
     @IBAction func dateBtnPressed(_ sender: Any) {
