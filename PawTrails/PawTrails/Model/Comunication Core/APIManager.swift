14fbc688873889de8fa908754ea67fceeed46558
Bruno Villanova <bruno@digimundi.com.br>
2017-12-13 13:44:33 -0200

    final fixes for build 1712131343

--- PawTrails/PawTrails/Model/Comunication Core/APIManager.swift
+++ PawTrails/PawTrails/Model/Comunication Core/APIManager.swift
@@ -115,16 +115,16 @@ class APIManager {
     static let instance = APIManager()
     
     //TODO: DO NOT FORGET! TO REMOVE THIS COMMENT!!
-//    #if DEBUG
-//    fileprivate static let mainURL = "https://eu.pawtrails.pet/api"
-//    fileprivate static let mainURLTest = "https://eu.pawtrails.pet/test"
-//    #else
-//    fileprivate static let mainURL = "https://eu.pawtrails.com/api"
-//    fileprivate static let mainURLTest = "https://eu.pawtrails.com/test"
-//    #endif
-    
+    #if DEBUG
+    fileprivate static let mainURL = "https://eu.pawtrails.pet/api"
+    fileprivate static let mainURLTest = "https://eu.pawtrails.pet/test"
+    #else
     fileprivate static let mainURL = "https://eu.pawtrails.com/api"
     fileprivate static let mainURLTest = "https://eu.pawtrails.com/test"
+    #endif
+//
+//    fileprivate static let mainURL = "https://eu.pawtrails.com/api"
+//    fileprivate static let mainURLTest = "https://eu.pawtrails.com/test"
     
     fileprivate let boundary = "%%%PawTrails%%%"
     
