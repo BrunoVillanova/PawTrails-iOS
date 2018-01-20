133636a8e832f3006dd7b2d975026679e2e9291e
Bruno Villanova <bruno@digimundi.com.br>
2018-01-20 00:44:16 -0200

    qr code scanner: navbar, close buttom, flash buttom, light scan on camera window

--- PawTrails/PawTrails/Controllers/QRCodeScannerViewController.swift
+++ PawTrails/PawTrails/Controllers/QRCodeScannerViewController.swift
@@ -17,15 +17,35 @@ public protocol QRCodeScannerViewControllerDelegate {
 class QRCodeScannerViewController: LBXScanViewController {
 
     var delegate: QRCodeScannerViewControllerDelegate?
+    var topTitle:UILabel?
+    var isOpenedFlash:Bool = false
+    var bottomItemsView:UIView?
+    var btnPhoto:UIButton = UIButton()
+    var btnFlash:UIButton = UIButton()
+    var btnMyQR:UIButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         initialize()
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        drawBottomItems()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation {
+        return UIStatusBarAnimation.slide
+    }
+
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
     
     override func handleCodeResult(arrayResult: [LBXScanResult]) {
@@ -36,7 +56,29 @@ class QRCodeScannerViewController: LBXScanViewController {
     
     fileprivate func initialize() {
         
+        self.extendedLayoutIncludesOpaqueBars = true
+        self.edgesForExtendedLayout = [.all]
+//        configureNavigatonBar()
+        
+        var style = LBXScanViewStyle()
+        style.animationImage = UIImage(named: "CodeScan.bundle/qrcode_scan_light_green")
+        self.scanStyle = style
+        
+        setNeedCodeImage(needCodeImg: true)
+        scanStyle?.centerUpOffset += 10
     }
+    
+//    fileprivate func configureNavigatonBar() {
+//        // Transparent navigation bar
+//        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+//        self.navigationController?.navigationBar.shadowImage = UIImage()
+//        self.navigationController?.navigationBar.isTranslucent = true
+//        self.navigationController?.navigationBar.backgroundColor = UIColor.clear
+//        self.navigationController?.navigationBar.tintColor = UIColor.white
+//        self.navigationController?.navigationBar.topItem?.title = " "
+//        self.navigationController?.navigationBar.backItem?.title = " "
+//        self.navigationItem.backBarButtonItem = UIBarButtonItem(title:"", style:.plain, target:nil, action:nil)
+//    }
 
     static func authorizeCameraWith(completion:@escaping (Bool) -> Void ) {
         
@@ -74,5 +116,72 @@ class QRCodeScannerViewController: LBXScanViewController {
         }
     }
     
+    
+    func drawBottomItems()
+    {
+        if (bottomItemsView != nil) {
+            
+            return;
+        }
+        
+        let yMax = self.view.frame.maxY - self.view.frame.minY
+        
+        bottomItemsView = UIView(frame:CGRect(x: 0.0, y: yMax-100,width: self.view.frame.size.width, height: 100 ) )
+        
+        
+        bottomItemsView!.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.6)
+        
+        self.view .addSubview(bottomItemsView!)
+        
+        
+        let size = CGSize(width: 65, height: 87);
+        
+        self.btnFlash = UIButton()
+        btnFlash.bounds = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        btnFlash.center = CGPoint(x: bottomItemsView!.frame.width/2, y: bottomItemsView!.frame.height/2)
+        btnFlash.setImage(UIImage(named: "CodeScan.bundle/qrcode_scan_btn_flash_nor"), for:UIControlState.normal)
+        btnFlash.addTarget(self, action: #selector(QRCodeScannerViewController.openOrCloseFlash), for: UIControlEvents.touchUpInside)
+        
+        
+//        self.btnPhoto = UIButton()
+//        btnPhoto.bounds = btnFlash.bounds
+//        btnPhoto.center = CGPoint(x: bottomItemsView!.frame.width/4, y: bottomItemsView!.frame.height/2)
+//        btnPhoto.setImage(UIImage(named: "CodeScan.bundle/qrcode_scan_btn_photo_nor"), for: UIControlState.normal)
+//        btnPhoto.setImage(UIImage(named: "CodeScan.bundle/qrcode_scan_btn_photo_down"), for: UIControlState.highlighted)
+//        //        btnPhoto.addTarget(self, action: Selector(("openPhotoAlbum")), for: UIControlEvents.touchUpInside)
+//
+//        btnPhoto.addTarget(self, action: #selector(QQScanViewController.openLocalPhotoAlbum), for: UIControlEvents.touchUpInside)
+//
+//
+//        self.btnMyQR = UIButton()
+//        btnMyQR.bounds = btnFlash.bounds;
+//        btnMyQR.center = CGPoint(x: bottomItemsView!.frame.width * 3/4, y: bottomItemsView!.frame.height/2);
+//        btnMyQR.setImage(UIImage(named: "CodeScan.bundle/qrcode_scan_btn_myqrcode_nor"), for: UIControlState.normal)
+//        btnMyQR.setImage(UIImage(named: "CodeScan.bundle/qrcode_scan_btn_myqrcode_down"), for: UIControlState.highlighted)
+//        btnMyQR.addTarget(self, action: #selector(QQScanViewController.myCode), for: UIControlEvents.touchUpInside)
+        
+        bottomItemsView?.addSubview(btnFlash)
+//        bottomItemsView?.addSubview(btnPhoto)
+//        bottomItemsView?.addSubview(btnMyQR)
+        
+        self.view .addSubview(bottomItemsView!)
+        
+    }
+    
+    @objc func openOrCloseFlash()
+    {
+        scanObj?.changeTorch();
+        
+        isOpenedFlash = !isOpenedFlash
+        
+        if isOpenedFlash
+        {
+            btnFlash.setImage(UIImage(named: "CodeScan.bundle/qrcode_scan_btn_flash_down"), for:UIControlState.normal)
+        }
+        else
+        {
+            btnFlash.setImage(UIImage(named: "CodeScan.bundle/qrcode_scan_btn_flash_nor"), for:UIControlState.normal)
+        }
+    }
 
 }
--- PawTrails/Tutorials/StepOneViewController.swift
+++ PawTrails/Tutorials/StepOneViewController.swift
@@ -11,55 +11,79 @@ import swiftScan
 
 class StepOneViewController: UIViewController {
 
+    var statusBarShouldBeHidden = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         initialize()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Show the status bar
+        statusBarShouldBeHidden = false
+        UIView.animate(withDuration: 0.25) {
+            self.setNeedsStatusBarAppearanceUpdate()
+        }
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return statusBarShouldBeHidden
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation {
+        return .slide
+    }
+    
     fileprivate func initialize() {
         self.navigationController?.navigationBar.backItem?.title = " "
-
-//        BarcodeScanner.Title.text = NSLocalizedString("Scan QR Code", comment: "")
-//        BarcodeScanner.CloseButton.text = NSLocalizedString("Close", comment: "")
-//        BarcodeScanner.SettingsButton.text = NSLocalizedString("Settings", comment: "")
-//        BarcodeScanner.Info.text = NSLocalizedString(
-//            "Place the QR code within the window to scan. The search will start automatically.", comment: "")
-//        BarcodeScanner.Info.loadingText = NSLocalizedString("Loading...", comment: "")
-//        BarcodeScanner.Info.notFoundText = NSLocalizedString("No product found.", comment: "")
-//        BarcodeScanner.Info.settingsText = NSLocalizedString(
-//            "To scan the QR Code you have to allow camera access under iOS settings.", comment: "")
-        
         if let nc = self.navigationController, nc.viewControllers.first == self {
             let closeBarButtonItem = UIBarButtonItem.init(title: "Close", style: UIBarButtonItemStyle.plain, target: self, action: #selector(self.closeViewController))
             self.navigationItem.setRightBarButton(closeBarButtonItem, animated: true)
         }
     }
     
-    
     @objc fileprivate func closeViewController() {
         self.dismiss(animated: true, completion: nil)
     }
-    
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        UIApplication.shared.statusBarStyle = .lightContent
-    }
-    
     
     @IBAction func scanNowBtnPressed(_ sender: Any) {
         QRCodeScannerViewController.authorizeCameraWith {[weak self](granted) in
             if granted, let strongSelf = self {
                 let vc = QRCodeScannerViewController();
                 vc.delegate = strongSelf;
-                var style = LBXScanViewStyle()
-                style.animationImage = UIImage(named: "CodeScan.bundle/qrcode_scan_light_green")
-                vc.scanStyle = style
-                strongSelf.present(vc, animated: true, completion: {
-                    UIApplication.shared.statusBarStyle = .default
+            
+                // Hide the status bar
+                strongSelf.statusBarShouldBeHidden = true
+                UIView.animate(withDuration: 0.25) {
+                    strongSelf.setNeedsStatusBarAppearanceUpdate()
+                }
+                
+                let navigationController = UINavigationController.init(rootViewController: vc)
+                navigationController.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+                navigationController.navigationBar.shadowImage = UIImage()
+                navigationController.navigationBar.isTranslucent = true
+                navigationController.navigationBar.backgroundColor = UIColor.clear
+                navigationController.navigationBar.tintColor = UIColor.white
+                
+                let barButtonItem = UIBarButtonItem.init(title: "Close", style: UIBarButtonItemStyle.plain, target:strongSelf, action: #selector(strongSelf.closeScannerViewController))
+                
+                vc.navigationItem.setRightBarButton(barButtonItem, animated: true)
+                
+                strongSelf.present(navigationController, animated: true, completion: {
+                    
                 })
             }
         }
+    }
+    
+    @objc fileprivate func closeScannerViewController() {
+        self.dismiss(animated: true, completion: nil)
     }
     
     fileprivate func goToNextStep(deviceCode: String) {
