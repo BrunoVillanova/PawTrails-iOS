1351fc003374d55530c9afca6b6d12325f0e9491
Bruno Villanova <bruno@digimundi.com.br>
2018-02-06 23:04:17 -0200

    fixed: address not persisting in pet list

--- PawTrails/PawTrails/Extensions/Extensions.swift
+++ PawTrails/PawTrails/Extensions/Extensions.swift
@@ -246,47 +246,8 @@ extension CGPoint {
     
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+extension Array where Element: Comparable {
+    func containsSameElements(as other: [Element]) -> Bool {
+        return self.count == other.count && self.sorted() == other.sorted()
+    }
+}
