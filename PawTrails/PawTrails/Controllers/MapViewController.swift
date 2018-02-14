3aa6d86a9d1ed16215f5d4f4965ed7b4f2653d1a
Abhijith <abhijith@attitudetech.ie>
2018-02-14 16:57:53 +0000

    Fixes related to tabbar

--- PawTrails/PawTrails/Controllers/MapViewController.swift
+++ PawTrails/PawTrails/Controllers/MapViewController.swift
@@ -47,6 +47,7 @@ class MapViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         mapView.showGpsUpdates()
+        self.tabBarController?.tabBar.isHidden = false
     }
     
     
