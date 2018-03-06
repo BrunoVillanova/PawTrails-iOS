9b6efe1149826d755fc19aa6a9fb2b7261ee1e50
Bruno Villanova <bruno@digimundi.com.br>
2018-03-06 14:07:06 -0300

    - fixed: layout and tab order in pet details - XLPagerTabStrip and SCLAlertView now use Carthage

--- PawTrails/PawTrails/Controllers/TabPageViewController.swift
+++ PawTrails/PawTrails/Controllers/TabPageViewController.swift
@@ -11,50 +11,17 @@ import XLPagerTabStrip
 
 class TabPageViewController: ButtonBarPagerTabStripViewController {
     
+    let color = UIColor(red: 206.0/255.0, green: 19.0/255.0, blue: 54.0/255.0, alpha: 1.0)
     var pet: Pet!
     var date: Date?
-    
-    
-    let color = UIColor(red: 206.0/255.0, green: 19.0/255.0, blue: 54.0/255.0, alpha: 1.0)
+    var iphoneX = false
     
     override func viewDidLoad() {
-        settings.style.buttonBarBackgroundColor = .white
-        self.automaticallyAdjustsScrollViewInsets = false
-        settings.style.buttonBarItemBackgroundColor = .white
-        settings.style.selectedBarBackgroundColor = .primary
-        settings.style.buttonBarItemTitleColor = .primary
-        settings.style.buttonBarItemFont = .systemFont(ofSize: 15)
-        settings.style.selectedBarHeight = 2.0
-        settings.style.buttonBarMinimumLineSpacing = 0
-        settings.style.buttonBarItemTitleColor = .black
-        settings.style.buttonBarItemsShouldFillAvailableWidth = true
-        settings.style.buttonBarLeftContentInset = 0
-        settings.style.buttonBarRightContentInset = 0
-        changeCurrentIndexProgressive = { [weak self] (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
-            guard changeCurrentIndex == true else { return }
-            oldCell?.label.textColor = .black
-            newCell?.label.textColor = self?.color
-        }
+        
+        initialize()
         super.viewDidLoad()
-        APIRepository.instance.loadPet(pet.id, callback: { (error, pet) in
-            if error == nil, let pet = pet {
-                self.pet = pet
-            }
-        })
-        
-        self.automaticallyAdjustsScrollViewInsets = false
-        
-        if pet.isOwner {
-            self.navigationItem.rightBarButtonItem =  UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(addTapped(_sender:)))
-        }
-        
-
-        navigationItem.title = pet.name
-//        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
-//        navigationController?.navigationBar.shadowImage = UIImage()
     }
     
-    var iphoneX = false
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -69,9 +36,45 @@ class TabPageViewController: ButtonBarPagerTabStripViewController {
 
     }
     
+    fileprivate func configureLayout() {
+        
+        automaticallyAdjustsScrollViewInsets = false
+        navigationItem.title = pet.name
+        
+        settings.style.buttonBarBackgroundColor = .white
+        settings.style.buttonBarItemBackgroundColor = .white
+        settings.style.selectedBarBackgroundColor = .primary
+        settings.style.buttonBarItemTitleColor = .primary
+        settings.style.buttonBarItemFont = UIFont(name: "Roboto-Medium", size:14)!
+        settings.style.selectedBarHeight = 2.0
+        settings.style.buttonBarMinimumLineSpacing = 0
+        settings.style.buttonBarItemTitleColor = .black
+        settings.style.buttonBarItemsShouldFillAvailableWidth = true
+        settings.style.buttonBarLeftContentInset = 0
+        settings.style.buttonBarRightContentInset = 0
+        
+        changeCurrentIndexProgressive = { [weak self] (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
+            guard changeCurrentIndex == true else { return }
+            oldCell?.label.textColor = .black
+            newCell?.label.textColor = self?.color
+        }
+        
+        if pet.isOwner {
+            self.navigationItem.rightBarButtonItem =  UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(editTapped(_:)))
+        }
+    }
     
-    func addTapped(_sender: UIBarButtonItem) {
-//        self.performSegue(withIdentifier: "editPetDetails", sender: self)
+    fileprivate func initialize() {
+        configureLayout()
+        
+        APIRepository.instance.loadPet(pet.id, callback: { (error, pet) in
+            if error == nil, let pet = pet {
+                self.pet = pet
+            }
+        })
+    }
+    
+    @objc fileprivate func editTapped(_ sender: UIBarButtonItem) {
         
         if let vc = self.storyboard?.instantiateViewController(withIdentifier: "AddEditPetDetailsTableViewController") as? AddEditPetDetailsTableViewController {
             if let pet = self.pet {
@@ -93,22 +96,21 @@ class TabPageViewController: ButtonBarPagerTabStripViewController {
         }
     }
 
-    
-    
-
     override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
-        let child_1 = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PetInfromationViewController") as! PetInfromationViewController
-        child_1.pet = self.pet
-        let SafeZoneViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "GoalsViewController") as! GoalsViewController
-        SafeZoneViewController.pet = self.pet
-        SafeZoneViewController.date = Date()
         
-        let child_3 = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SafeZoneViewController") as! SafeZoneViewController
-        child_3.pet = self.pet
+        let petInfo = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PetInfromationViewController") as! PetInfromationViewController
+        petInfo.pet = self.pet
         
-        let child_4 = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "AdventuresListViewController") as! AdventuresListViewController
-        child_4.pet = self.pet
-        return [child_1, SafeZoneViewController, child_3, child_4]
+        let goals = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "GoalsViewController") as! GoalsViewController
+        goals.pet = self.pet
+        goals.date = Date()
+        
+        let safeZone = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SafeZoneViewController") as! SafeZoneViewController
+        safeZone.pet = self.pet
+        
+        let adventureHistory = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "AdventuresListViewController") as! AdventuresListViewController
+        adventureHistory.pet = self.pet
+        
+        return [petInfo, goals, adventureHistory, safeZone]
     }
-
 }
