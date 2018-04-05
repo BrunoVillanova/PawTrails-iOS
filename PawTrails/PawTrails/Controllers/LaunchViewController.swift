403e206abc6279a11c91a094d67e01b14b6e2c43
Bruno Villanova <bruno@attitudetech.ie>
2018-04-05 19:55:51 -0300

    added: launch view controller images for each device screen

--- PawTrails/PawTrails/Controllers/LaunchViewController.swift
+++ PawTrails/PawTrails/Controllers/LaunchViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class LaunchViewController: UIViewController {
 
+    @IBOutlet weak var launchImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         initialize()
@@ -26,15 +28,39 @@ class LaunchViewController: UIViewController {
     }
     
     fileprivate func initialize() {
-
+        
+        var imageName: String?
+        
+        switch UIDevice.current.screenType {
+            case .iPhone4_4S:
+                imageName = "LaunchImage-iPhone4S"
+            case .iPhones_5_5s_5c_SE:
+                imageName = "LaunchImage-iPhone5"
+            case .iPhones_6_6s_7_8:
+                imageName = "LaunchImage-iPhone8"
+            case .iPhones_6Plus_6sPlus_7Plus_8Plus:
+                imageName = "LaunchImage-iPhone8Plus"
+            case .iPhoneX:
+                imageName = "LaunchImage-iPhoneX"
+            default:
+                break
+        }
+        
+        if let imageName = imageName, let image = UIImage(named: imageName) {
+            launchImageView.image = image
+            launchImageView.contentMode = .center
+        }
+        
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         
         if DataManager.instance.isAuthenticated() {
             // Workaround: calling this method to validate token and check account verification status
-
+            self.showLoadingView()
             APIRepository.instance.loadPets { (error, pets) in
 
-                if let error = error, error.httpCode == 401 {
+                self.hideLoadingView()
+                
+                if let error = error {
                     
                     appDelegate.loadAuthenticationScreen()
     
