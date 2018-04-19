1853b0348068d35457b49e0c398331c8197bac35
Abhijith <abhijith@attitudetech.ie>
2018-04-19 12:03:58 +0100

    Adventure start page not displaying : FIXED About us page new Layout added

--- PawTrails/PawTrails/Controllers/AboutPawTrails.swift
+++ PawTrails/PawTrails/Controllers/AboutPawTrails.swift
@@ -11,18 +11,34 @@ import MessageUI
 
 class AboutPawTrails: UIViewController {
 
-    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if #available(iOS 11.0, *) {
-            scrollView.contentInsetAdjustmentBehavior = .never
-        } else {
-            automaticallyAdjustsScrollViewInsets = false
-        }
-    }
 
-    @IBAction func visitUsPressed(_ sender: Any) {
+        configureNavBar()
+        
+     tableView.tableFooterView = UIView.init(frame: CGRect.zero)
+        
+    }
+    
+    
+    func configureNavBar() {
+        
+        self.title = "About PawTrails"
+        let attributes = [NSFontAttributeName: UIFont(name: "Montserrat-Regular", size: 18)!,NSForegroundColorAttributeName: PTConstants.colors.darkGray]
+        UINavigationBar.appearance().titleTextAttributes = attributes
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "BackIcon"), style: .plain, target: self, action: #selector(backButtonTapped))
+        self.navigationItem.leftBarButtonItem?.tintColor = PTConstants.colors.darkGray
+    }
+    
+    func backButtonTapped() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    
+    //MARK: Private methods
+
+     fileprivate func visitUsPressed() {
         
         if let urlFromStr = URL(string: "https://www.pawtrails.ie") {
             if UIApplication.shared.canOpenURL(urlFromStr) {
@@ -36,7 +52,7 @@ class AboutPawTrails: UIViewController {
     
     }
     
-    @IBAction func contactUsBtnPressed(_ sender: Any) {
+     fileprivate func contactUsBtnPressed() {
         if let urlFromStr = URL(string: "mailto:info@pawtrails.ie") {
             if UIApplication.shared.canOpenURL(urlFromStr) {
                 if #available(iOS 10.0, *) {
@@ -54,7 +70,7 @@ class AboutPawTrails: UIViewController {
     
     
     
-    @IBAction func facebookBtnPressed(_ sender: Any) {
+     fileprivate func facebookBtnPressed() {
         if let urlFromStr = URL(string: "fb://profile/1245888162113500") {
             if UIApplication.shared.canOpenURL(urlFromStr) {
                 if #available(iOS 10.0, *) {
@@ -72,7 +88,8 @@ class AboutPawTrails: UIViewController {
         
         
     }
-    @IBAction func instagramBtnPressed(_ sender: Any) {
+    
+     fileprivate func instagramBtnPressed() {
         if let urlFromStr = URL(string: "instagram://user?username=pawtrails_smartcollar") {
             if UIApplication.shared.canOpenURL(urlFromStr) {
                 if #available(iOS 10.0, *) {
@@ -87,7 +104,7 @@ class AboutPawTrails: UIViewController {
         }
     }
     
-    @IBAction func youtubeBtnPressed(_ sender: Any) {
+    fileprivate func youtubeBtnPressed() {
         let youtubeId = "UCbHl4YKaS7UpxMHdgl5TPfQ"
         if let urlFromStr = URL(string: "youtube://\(youtubeId)") {
             if UIApplication.shared.canOpenURL(urlFromStr) {
@@ -104,7 +121,7 @@ class AboutPawTrails: UIViewController {
         
         
     }
-    @IBAction func rateUsOnBtnPressed(_ sender: Any) {
+     fileprivate func rateUsOnBtnPressed() {
         
             if let urlFromStr = URL(string: "https://itunes.apple.com/gb/app/pawtrails/id1321502523?mt=8&ign-mpt=uo%3D2") {
             if UIApplication.shared.canOpenURL(urlFromStr) {
@@ -119,4 +136,75 @@ class AboutPawTrails: UIViewController {
             }
         }
     }
+}
+
+extension AboutPawTrails: UITableViewDelegate,UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 5
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? AboutUsCell{
+            
+            switch indexPath.row {
+            case 0:
+                cell.titleLabel.text = "Visit PawTrails Website"
+                cell.iconImage.image = UIImage(named:"WechatIMG135")
+            case 1:
+                cell.titleLabel.text = "Like us on Facebook"
+                cell.iconImage.image = UIImage(named:"facebook")
+            case 2:
+                cell.titleLabel.text = "Follow us on Instagram"
+                cell.iconImage.image = UIImage(named:"instagram")
+            case 3:
+                cell.titleLabel.text = "Watch us on Youtube"
+                cell.iconImage.image = UIImage(named:"youtube")
+            case 4:
+                cell.titleLabel.text = "Rate us on App Store"
+                cell.iconImage.image = UIImage(named:"app-store")
+            default:
+                break
+            }
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "nothing", for: indexPath)
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        switch indexPath.row {
+        case 0:
+            visitUsPressed()
+        case 1:
+            facebookBtnPressed()
+        case 2:
+            instagramBtnPressed()
+        case 3:
+            youtubeBtnPressed()
+        case 4:
+            rateUsOnBtnPressed()
+        default:
+            break
+        }
+    }
+    
+}
+
+class AboutUsCell : UITableViewCell {
+    
+    @IBOutlet weak var iconImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
 }
